32d7a342c1fe97436332eb8e67a5c3fa3e4c273f
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-20 21:44:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,9 @@
-class Dog 
-  
-  def name=(dogs_name)
-    @name = dogs_name
+class Dog
+  def name=(dog_name)
+    @this_dogs_name = dog_name
   end
-  
+
   def name
-    @name 
+    @this_dogs_name
   end
-  
-end 
+end
